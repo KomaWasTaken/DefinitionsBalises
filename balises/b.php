fdf581c1c0cc0a7be9218436b90bb4226c2b3788
r0m21 <romainwenger21@gmail.com>
2018-05-18 11:32:46 +0200

    HTML terminé pour la partie 'Structuration de texte' + init style.css

---
+++ balises/b.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Balise &lt;b&gt;</title>
+</head>
+<body>
+    <?php include "../nav.php" ?>
+
+<section>
+    <h2>La balise &lt;b&gt;</h2>
+    <p>La balise &lt;b&gt; est une balise fermante, ce qu'elle encadre sera retranscrit en gras.</p>
+    <h5>Voici un exemple d'utilisation de la balise &lt;b&gt;> :</h5>
+    <img src="../images/baliseb.png" alt="Exemple balise bold">
+    <h5>Voici un exemple de ce que l'utilisateur apercevera :</h5>
+    <p>This is a normal text - <b>and this is a bold text </b>.</p>
+
+    <aside>
+        <a href="https://www.w3schools.com/tags/tag_b.asp">Source obtenue sur W3C.</a>
+    </aside>
+
+</section>
+
+    <?php include "../footer.php" ?>
+</body>
+</html>
---
+++ balises/hn.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Balise &lt;hn&gt;</title>
+</head>
+<body>
+    <?php include "../nav.php" ?>
+
+<section>
+    <h2>La balise &lt;hn&gt;</h2>
+    <p>
+        Dans une page web, tout les titres sont construit entre des balises &lt;hn&gt; ( h = heading en anglais ),
+        n étant un chiffre de 1 à 6 et ont ces 2 principales caractéristiques :
+    </p>
+    <p>
+        Le référencement ! Il est très important car il est souvent constitué de différent mot clé de votre contenu qui suit, les moteurs de recherche, recherche toutes les informations les plus importante de votre page ( Google accorderait plus de poids aux mots clés et expression apparaissant dans votre titre qu'au reste du contenu )
+        L'ergonomie ! Cela fait bel et bien partit de la fluidité du site en général. Le titre résume sur quel sujet votre contenu va se porter, donc l'utilisateur sera beaucoup plus rapide pour faire ces recherches.
+        Il existe 6 niveaux de titrage possibles : h1, h2, h3, h4, h5 et h6. h1 étant un titre de premier niveau, h2 un titre de second niveau, h3, un titre de troisième niveau etc... Il est important d'être cohérent sur l'importance de vos titres.
+    </p>
+    <h5 class="">Voici l'exemple d'un titre :</h5>
+    
+    <h1>Voici un exemple de titre h1</h1>
+    <h2>Voici un exemple de titre h2</h2>
+    <h3>Voici un exemple de titre h3</h3>
+    <h4>Voici un exemple de titre h4</h4>
+    <h5>Voici un exemple de titre h5</h5>
+    <h6>Voici un exemple de titre h6</h6>
+
+    <aside>
+        <a href="https://www.yakaferci.com/balises-h1-h2/">Source obtenue sur Yakaferci</a>
+    </aside>
+</section>
+
+    <?php include "../footer.php" ?>
+</body>
+</html>
