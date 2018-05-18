502cef2e68648f9d3c0680977db8b45d742a26c2
chillisthx <l.pasionek@gmail.com>
2018-05-18 10:43:11 +0200

    nettoyage des pages html head et body

---
+++ body.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css"/>
+    <title>Définition de la balise &lt;body&gt;</title> <!--Ne pas toucher-->
+</head>
+
+<body>
+    <?php include 'nav.php'; ?>
+
+    <section>
+    La balise &lt;body&gt;
+    
+    La balise &lt;body&gt; est ce qui définit le corps d'un document,
+    ce qui contient l'information destinée à être affichée.
+    Elle contient tout le contenu d'un document HTML5, que ce soit une image, un hyperlien, un tableau etc ...
+    La balise &lt;body&gt; prend en charge tous les attributs d'événement HTML5.
+    
+    Exemple :
+    <img src="images/body.jpg" alt="Exemple d'utilisation de balise body" />
+
+    Source : <a href="http://41mag.fr/liste-des-balises-html5/balise-body-html5" target="_blank">41mag</a>
+    
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
