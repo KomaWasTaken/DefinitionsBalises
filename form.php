0134bc77ddf1d4989b037a5f4488852c41d43190
Jim Pronesti <drak3r21@hotmail.com>
2018-05-18 10:49:00 +0200

    Nettoyage de page et ajout du fichier CSS
    Nettoyage des pages div, form, img, input, link, meta, nav, table et title

---
+++ form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Form</title>
+</head>
+
+<body>
+    <?php include 'nav.php' ?>
+
+    <section>
+            <h2>La balise &lt;form&gt;</h2>
+            <p>La balise <strong>&lt;form&gt;</strong> est utilisée pour créer un formulaire HTML5.<br>
+            Elle peut afficher des éléments sous la forme d'un <a href="http://41mag.fr/liste-des-balises-html5/balise-button-html5" target="_blank">bouton</a>, d'une <a href="http://41mag.fr/liste-des-balises-html5/balise-datalist-html5" target="_blank">liste</a>, d'une <a href="http://www.startyourdev.com/html/tag-html-balise-textarea" target="_blank">zone de texte</a> etc.. selon les éléments qu'elle contient.<br></p>
+            <p>Voici les attributs que peut prendre la balise :<br> <br>
+            <img src="images/form_balise.PNG" alt="attributs et valeurs possibles de form"></p>
+        
+        <aside>
+            <p><i>Source :</i> <a href="http://41mag.fr/liste-des-balises-html5/balise-form-html5" target="_blank">41mag</a></p>
+        </aside>
+    </section>
+
+    <?php include 'footer.php' ?>
+</body>
+</html>
---
+++ link.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Link</title>
+</head>
+
+<body>
+<?php include 'nav.php' ?>
+
+    <section>
+            <h2>La balise &lt;link&gt;</h2>
+
+            <p> L'élément HTML <strong>&lt;link&gt;</strong> définit la relation entre le document courant et une ressource externe.<br/>
+                Cet élément peut être utilisé pour <strong>définir un lien</strong> vers une feuille de style ou un cadre de navigation (accéder à la même page dans une langue différente par exemple). <!--Description de la balise + ajout d'un exemple et image si possible-->
+                Il ne peut être inséré que dans &lt;head&gt;.
+            </p>
+
+            <p>
+                <u>Exemples :</u><br/><br/>
+                Pour associer une feuille de style à la page courante, on utilisera la syntaxe suivante :<br/>
+                    <img src="images/exemple_link.png" alt="Exemple de balise &lt;link&gt;"/><br/><br/>
+
+                Pour un document, on peut indiquer plusieurs feuilles de style alternatives.<br/>
+
+                L'utilisateur pourra choisir parmi ces feuilles de style via le menu « Affichage > Style de la page ».<br/>
+                Ainsi, un utilisateur pourra voir différentes versions d'une même page.<br/>
+                    <img src="images/exemple_link2.png" alt="Exemple de balise &lt;link&gt;"/><br/>
+            </p>
+        
+        <aside>
+            <i>Source du texte :  </i><a href="http://41mag.fr/liste-des-balises-html5/balise-a-html5-lien" target="_blank">41mag.fr</a>
+        </aside>
+    </section>
+
+<?php include 'footer.php' ?>
+</body>
+</html>
