01d70cb1cfe4a8d8ebf9227cc6e536db30804426
r0m21 <romainwenger21@gmail.com>
2018-05-18 10:22:15 +0200

    init projet

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="flex jccenter bgcolor3 ptb-80">
+    <section class="flex fd-column">
+        <div class="flex jcaround">
+                <a class="decorationnone" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook color4"></i></a>
+          
+                <a class="decorationnone" href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter-square color4"></i></a>
+            
+                <a class="decorationnone" href="https://www.linkedin.com/" alt="Linkedin" target="_blank"><i class="fab fa-linkedin color4"></i></a>
+            
+                <a class="decorationnone" href="formulaire.php"><i class="fab fa-wpforms color4"></i></a>
+        </div>
+        
+        <div class="flex jccenter pt-30">
+            <p class="color4 biter fs-20">Copyright © 2018 Troll A+ en lan</p>
+        </div>
+</section>
+</footer>
