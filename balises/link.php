8b468c462ba0e41cff0f9fcbf3955bfc7afc8579
KomaWasTaken <valentin.gaillard-21@outlook.fr>
2018-05-18 16:35:07 +0200

    refonte html Balises d en tête

--- balises/link.php
+++ balises/link.php
@@ -13,28 +13,36 @@
 <body>
 <?php include '../include/header.php' ?>
 
-    <section>
-            <h2>La balise &lt;link&gt;</h2>
+    <section class="container-fluid color2 bgcolor4 ptb-50">
+        <div class="container">
+            <div class="row flex-column">
+                <h2 class="fs-30">La balise &lt;link&gt;</h2>
 
-            <p> L'élément HTML <strong>&lt;link&gt;</strong> définit la relation entre le document courant et une ressource externe.
-                Cet élément peut être utilisé pour <strong>définir un lien</strong> vers une feuille de style ou un cadre de navigation (accéder à la même page dans une langue différente par exemple). <!--Description de la balise + ajout d'un exemple et image si possible-->
-                Il ne peut être inséré que dans &lt;head&gt;.
-            </p>
+                <p class="fs-15 col-12"> L'élément HTML <strong>&lt;link&gt;</strong> définit la relation entre le document courant et une ressource externe.
+                    Cet élément peut être utilisé pour <strong>définir un lien</strong> vers une feuille de style ou un cadre de navigation (accéder à la même page dans une langue différente par exemple). <!--Description de la balise + ajout d'un exemple et image si possible-->
+                    Il ne peut être inséré que dans &lt;head&gt;.
+                </p>
 
+                
+                    <h5 class="fs-20">Exemples :</h5>
+                <p class="fs-15 col-12">Pour associer une feuille de style à la page courante, on utilisera la syntaxe suivante :</p>
+                    <div class="col-12">
+                        <img src="../images/exemple_link.png" class="img-fluid" alt="Exemple de balise &lt;link&gt;"/>
+                    </div>
+
+                <p class="fs-15 col-12"> Pour un document, on peut indiquer plusieurs feuilles de style alternatives.
+                    L'utilisateur pourra choisir parmi ces feuilles de style via le menu « Affichage > Style de la page ».
+                    Ainsi, un utilisateur pourra voir différentes versions d'une même page.
+                </p>
+                <div class="col-12">                
+                    <img src="../images/exemple_link2.png" class="img-fluid" alt="Exemple de balise &lt;link&gt;"/>
+                </div>
             
-                <h5>Exemples :</h5>
-            <p>Pour associer une feuille de style à la page courante, on utilisera la syntaxe suivante :</p>
-                <img src="images/exemple_link.png" alt="Exemple de balise &lt;link&gt;"/>
-
-            <p> Pour un document, on peut indiquer plusieurs feuilles de style alternatives.
-                L'utilisateur pourra choisir parmi ces feuilles de style via le menu « Affichage > Style de la page ».
-                Ainsi, un utilisateur pourra voir différentes versions d'une même page.
-            </p>
-            <img src="images/exemple_link2.png" alt="Exemple de balise &lt;link&gt;"/>
-        
-        <aside>
-            <i>Source du texte : </i><a href="http://41mag.fr/liste-des-balises-html5/balise-a-html5-lien" target="_blank">41mag</a>
-        </aside>
+                <aside class="fs-20">
+                    <i>Source du texte : </i><a href="http://41mag.fr/liste-des-balises-html5/balise-a-html5-lien" target="_blank">41mag</a>
+                </aside>
+            </div>
+        </div>
     </section>
 
 <?php include '../include/footer.php' ?>
--- balises/title.php
+++ balises/title.php
@@ -13,24 +13,30 @@
 <body>
     <?php include '../include/header.php' ?>
 
-    <section>
-            <h2>La balise &lt;title&gt;</h2>
+    <section class="container-fluid color2 bgcolor4 ptb-50">
+        <div class="container">
+            <div class=" row flex-column">
+            <h2 class="fs-30">La balise &lt;title&gt;</h2>
 
-            <p>L'élément <strong>&lt;title&gt;</strong> définit le <strong>titre du document</strong> (qui est affiché dans la barre de titre du navigateur ou dans l'onglet de la page).
-            Cet élément ne peut contenir que du texte, les balises qu'il contiendrait seraient ignorées.</p>
+            <p class="fs-15 col-12">
+                L'élément &lt;title&gt; définit le titre du document (qui est affiché dans la barre de titre du navigateur ou dans l'onglet de la page).
+                Cet élément ne peut contenir que du texte, les balises qu'il contiendrait seraient ignorées.
+            </p>
             
-            <h5>Exemple :</h5>
-            <p><cite><strong>&lt;title&gt;</strong>Et voici le titre de ma page !<strong>&lt;/title&gt;</strong></cite></p>
+            <h5 class="fs-20">Exemple :</h5>
+            <p class="fs-15 col-12">&lt;title&gt;Et voici le titre de ma page !&lt;/title&gt;</p>
                 
-            <p>Le fait de renseigner et d’optimiser la balise title est indispensable car cela joue un grand rôle dans le domaine du référencement naturel.
+            <p class="fs-15 col-12">Le fait de renseigner et d’optimiser la balise title est indispensable car cela joue un grand rôle dans le domaine du référencement naturel.
             Etant unique, la balise title est considérée logiquement par Google et par les autres moteur comme un élément pertinent et significatif pour déterminer la thématique principale d’une page. 
             La balise title est probablement un des critères de référencement on-page ayant le plus de poids.
             Un mot clé a d’autant plus de poids comme balise titre lorsqu’il figure seul ou en début de balise.
             </p>
 
-        <aside><i>Sources : </i><a href="https://developer.mozilla.org/fr/docs/Web/HTML/Element/title" target="_blank">Mozilla</a>
+        <aside class="fs-20">
+            <i>Sources : </i><a href="https://developer.mozilla.org/fr/docs/Web/HTML/Element/title" target="_blank">Mozilla</a>
             <a href="https://www.definitions-marketing.com/definition/balise-title/" target="_blank">Définitions-marketing</a>
         </aside>
+        </div>
     </section>
 
     <?php include '../include/footer.php' ?>
